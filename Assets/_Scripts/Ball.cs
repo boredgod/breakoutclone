db63050f466af90d367286889d349be04cebc009
boredgod <saikrishguna@outlook.com>
2025-12-31 21:51:01 +0530

    Generator for brick placement, ball bounce interactions
    Made the ball bounce deflect in certain angles based on contact angles and contact side of paddle. Followed a tutorial for it, Added a tile procedural generation script from another tutorial. Have to tweak it to place bricks based on cell world position

--- Assets/_Scripts/Ball.cs
+++ Assets/_Scripts/Ball.cs
@@ -5,6 +5,8 @@ public class Ball : MonoBehaviour
     [SerializeField] float initialSpeed = 5f;
     [SerializeField] float minSpeed = 4f;
     [SerializeField] float maxSpeed = 8f;
+    
+    [SerializeField] float maxBounceAngle = 75;
     Rigidbody2D rb;
     bool isLaunched = false;
     GameObject parentPaddle;
@@ -50,6 +52,26 @@ public class Ball : MonoBehaviour
         rb.linearVelocity = Vector2.zero;
         transform.SetParent(parentPaddle.transform);
         transform.localPosition = initialLocalPostion;
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        BallReflect(collision);
+    }
+    private void BallReflect(Collision2D collision)
+    {        
+        if (collision.gameObject.CompareTag("Player") && isLaunched)
+        {
+            Vector3 paddlePosition = collision.transform.position;
+            Vector2 contactPoint = collision.GetContact(0).point;
+            float offset = paddlePosition.x - contactPoint.x;
+            float width = collision.collider.bounds.size.x / 2f;
+            float currentAngle = Vector2.SignedAngle(Vector2.up, rb.linearVelocity);
+            float bounceAngle = (offset / width) * maxBounceAngle;
+            float newAngle = Mathf.Clamp(currentAngle + bounceAngle, -maxBounceAngle, maxBounceAngle);
+
+            Quaternion rotation = Quaternion.AngleAxis(newAngle, Vector3.forward);
+            rb.linearVelocity = rotation * Vector2.up * rb.linearVelocity.magnitude;
+        }
     }
 
 }
---
+++ Assets/_Scripts/Tilegen/TileAutomator.cs
@@ -0,0 +1,136 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Tilemaps;
+
+public class TileAutomator : MonoBehaviour
+{
+    [Range(0,100)][SerializeField] int initialChance;
+    [Range(1,8)][SerializeField] int birthLimit;
+    [Range(1,8)][SerializeField] int deathLimit;
+    [Range(1,10)][SerializeField] int numRepeat;
+    int count = 0;
+    int[,] terrainMap;
+    [SerializeField] Vector3Int tMapSize;
+
+    [SerializeField]Tilemap topMap;
+    [SerializeField]Tilemap bottomMap;
+    [SerializeField]Tile topTile;
+    [SerializeField] Tile bottomTile;
+
+    int width;
+    int height;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DoSimulation(numRepeat);
+        // ClearMaps(true); 
+    }
+
+    void DoSimulation(int numRepeat)
+    {
+        ClearMaps(false);
+        width = tMapSize.x;
+        height = tMapSize.y;
+
+        if (terrainMap == null)
+        {
+            terrainMap = new int[width, height];
+            InitialPosition();
+        }
+
+        for(int i=0; i < numRepeat; i++)
+        {
+            terrainMap = GenerateTilePosition(terrainMap);
+        }
+        for(int i = 0; i < width; i++)
+        {
+            for(int j = 0; j < height; j++)
+            {
+                if(terrainMap[i,j] == 1)
+                {
+                    topMap.SetTile(new Vector3Int(-i + width/2, -j + height/2, 0), topTile);
+                    bottomMap.SetTile(new Vector3Int(-i + width/2, -j + height/2, 0), bottomTile);
+                }
+            }
+        }
+    }
+    int[,] GenerateTilePosition(int[,] oldMap)
+    {
+        int[,] newMap = new int[width, height];
+        int neighbors;
+        BoundsInt myBounds = new BoundsInt(-1, -1, 0, 3, 3, 1);
+        for(int i = 0; i < width; i++)
+        {
+            for(int j = 0; j < height; j++)
+            {
+                neighbors = 0;
+                foreach( var b in myBounds.allPositionsWithin)
+                {
+                    if(b.x == 0 && b.y == 0)
+                    {
+                        continue;
+                    }
+                    if(i + b.x >= 0 && i+b.x < width && j+b.y>= 0 && j+b.y < height)
+                    {
+                        neighbors += oldMap[i+b.x, j+b.y];// getting neighbors from oldmap
+                    }
+                    else
+                    {
+                        neighbors++;//border for terrain
+                    }
+                }
+
+                if( oldMap[i,j] == 1)
+                {
+                    if(neighbors < deathLimit)
+                    {
+                        newMap[i,j] = 0;
+                    }
+                    else
+                    {
+                        newMap[i,j] = 1;
+                    }
+                }
+                if(oldMap[i,j] == 0)
+                {
+                    if(neighbors > birthLimit)
+                    {
+                        newMap[i,j] = 1;
+                    }
+                    else
+                    {
+                        newMap[i,j] = 0;
+                    }
+                }
+
+            }
+        }
+
+        return newMap;
+    }
+    void InitialPosition()
+    {
+        for(int i = 0; i < width; i++)
+        {
+            for(int j = 0; j < height; j++)
+            {
+                terrainMap[i,j] = Random.Range(1,101) < initialChance ? 1 : 0;
+            }
+        }
+    }
+    void ClearMaps(bool complete)
+    {
+        topMap.ClearAllTiles();
+        bottomMap.ClearAllTiles();
+        if(complete)
+        {
+            terrainMap = null;
+        }
+    }
+}
